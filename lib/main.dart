8d641b9c26068818772d2c22edd25991d054fcb9
Farhan <muhdfarhan1097@gmail.com>
2021-10-28 00:13:14 +0800

    not here

---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        home: MyHomePage(
+      title: 'Hello',
+    ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
+  var currentSelectedValue;
+  static const genderTypes = ["Male", "Female", "Others"];
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign Up Page'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          children: [
+            Container(
+              child: const Center(
+                child: Text(
+                  'WELCOME!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+                child: const Image(
+              image: NetworkImage(
+                  'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/60c2c9c4-c5db-443a-ba53-0acc0a5875e7/d6rix0y-15ae5f98-07e3-44fb-bd82-903c1c131167.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzYwYzJjOWM0LWM1ZGItNDQzYS1iYTUzLTBhY2MwYTU4NzVlN1wvZDZyaXgweS0xNWFlNWY5OC0wN2UzLTQ0ZmItYmQ4Mi05MDNjMWMxMzExNjcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.67CoRyuj9PQyCA6k9WrP_iwX0CNy1TF0jiN75eRaJb8'),
+              height: 200,
+            )),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your full name';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Full Name',
+                    hintText: 'Enter your full name as indicated in your IC'),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter a valid mail id such as abc@gmail.com'),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a password';
+                  }
+                  return null;
+                },
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter your secure password'),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(18),
+              width: 600,
+              child: DropdownButtonFormField<String>(
+                validator: (value) => value == null ? 'Please select your gender' : null,
+                isExpanded: true,
+                hint: Text("Select your gender"),
+                value: currentSelectedValue,
+                icon: Icon(Icons.keyboard_arrow_down_rounded),
+                items: genderTypes.map((String value) {
+                  return DropdownMenuItem(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    currentSelectedValue = newValue;
+                  });
+                },
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                       SnackBar(
+                        content: Text('You have successfully signed up!'),
+                        action: SnackBarAction(label: 'OK', onPressed: () {}, 
+                        ),
+                    )
+                    );
+                  }
+                },
+                child: Text('Sign Up')
+                )
+          ],
+        ),
+      ),
+    );
+  }
+}
