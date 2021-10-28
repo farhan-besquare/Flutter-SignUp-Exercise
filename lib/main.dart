a26b9b4792fd5a093a1bb366834ebd7445afe1d8
Farhan <muhdfarhan1097@gmail.com>
2021-10-28 10:30:54 +0800

    theme color

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
         home: MyHomePage(
       title: 'Hello',
     ));
